f175f5c5e47ecce19019b43f445282b15ed1000e
Quadrotez <quadrotez@proton.me>
2024-11-04 01:33:15 +0300

    feat: add content in index.html, script.js, styles.css

--- script.js
+++ script.js
@@ -6,26 +6,18 @@ const myEmail = document.getElementById("myEmail")
 
 function addlistener(x) {
     x.addEventListener('mouseenter', function() {
-
         let max = 10
         let min = -10
         
-        
         function randomInteger (min, max) {
             return Math.floor(Math.random() * (max - min + 1)) + min;
-        }
-        
-        
-        
+        }        
         this.style.transform = `translateY(${randomInteger(min, max)}px) translateX(${randomInteger(min, max)}px)`;
         });
 }
 
-addlistener(myGitHub)
-addlistener(myKwork)
-addlistener(myTelegram)
-addlistener(myDiscord)
-addlistener(myEmail)
+for (let i of [myGitHub, myKwork, myTelegram, myDiscord, myEmail]) {addlistener(i)}
+
 
 var map = L.map('map').setView([55.7558, 37.6173], 10);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -33,3 +25,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 var marker = L.marker([55.7558, 37.6173]).addTo(map);
+
+
+
+
